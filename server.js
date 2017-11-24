5cacfab1d1a417e4e4dd0f96bcbeaa1db7782a0e
Max Nordlund gmail <max.nordlund@gmail.com>
2017-11-24 11:33:34 +0100

    Add postcss support

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+const path = require("path")
+
 // Serve local files on localhost:3000
 require("connect")()
   .use(require("morgan")("dev"))
+  .use("/styles", require("postcss-middleware")({
+    plugins: [require("postcss-cssnext")({
+      //
+    })],
+    src(req) {
+      return `src/${req.url}`
+    }
+  }))
   .use(require("serve-static")(`${__dirname}/app`))
   .listen(3000)
 
 // Watch for changes for live reload
 require("livereload")
-  .createServer()
-  .watch(`${__dirname}/app`)
+  .createServer({
+    applyCSSLive: false
+  })
+  .watch([`${__dirname}/app`, `${__dirname}/src`])
